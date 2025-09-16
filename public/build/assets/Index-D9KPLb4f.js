33259311a11578304b80f0c06327aa8d10923099
Rusaln-IT <ruslan.r.f@yandex.ru>
2025-09-16 11:32:57 +0500

    Build assets for production

--- public/build/assets/Index-D9KPLb4f.js
+++
@@ -1,1 +0,0 @@
-import{A as t}from"./AdminLayout-Bt8Ooeax.js";import{_ as o}from"./_plugin-vue_export-helper-DlAUqK2U.js";import"./app-CzakFkXn.js";const r={name:"Index",layout:t};function e(n,a,s,c,p,_){return" Продукты "}const i=o(r,[["render",e]]);export{i as default};
