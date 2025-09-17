232ea7be7da8d027b00f1910410530eb00bb933c
Rusaln-IT <ruslan.r.f@yandex.ru>
2025-09-17 23:21:29 +0500

    Build assets for producsdsdion

---
+++ public/build/assets/Index-DImjHlXE.js
@@ -0,0 +1,1 @@
+import{A as t}from"./AdminLayout-BjwjRWSQ.js";import{_ as o}from"./_plugin-vue_export-helper-DlAUqK2U.js";import"./app-BQBYSx4T.js";const r={name:"Index",layout:t};function e(n,a,s,c,p,_){return" Продукты "}const i=o(r,[["render",e]]);export{i as default};
