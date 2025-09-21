e18263f34b2b376348fd5f61a8484a45f8628b03
Rusaln-IT <ruslan.r.f@yandex.ru>
2025-09-21 22:53:26 +0500

    Build assets for production

--- public/build/assets/Index-Bno29DWh.js
+++
@@ -1,1 +0,0 @@
-import{A as o}from"./AdminLayout-C_zAxJPi.js";import{_ as t}from"./_plugin-vue_export-helper-DlAUqK2U.js";import"./app-B_PwAWjU.js";const r={name:"Index",layout:o};function a(e,n,s,c,d,p){return" dashboard "}const i=t(r,[["render",a]]);export{i as default};
