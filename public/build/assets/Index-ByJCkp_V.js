9aef926b66d0034d9564f84b9917d0a823bbf9b9
Rusaln-IT <ruslan.r.f@yandex.ru>
2025-09-22 16:55:05 +0500

    Build assets for production

---
+++ public/build/assets/Index-ByJCkp_V.js
@@ -0,0 +1,1 @@
+import{A as o}from"./AdminLayout-DUVRi72Y.js";import{_ as t}from"./_plugin-vue_export-helper-DlAUqK2U.js";import"./app-3Vl6Jjj1.js";const r={name:"Index",layout:o};function a(e,n,s,c,d,p){return" dashboard "}const i=t(r,[["render",a]]);export{i as default};
